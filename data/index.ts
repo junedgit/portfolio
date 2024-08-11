e035932662420a4e0d5087538bfc735a098f31a7
JUNED <76807263+junedgit@users.noreply.github.com>
2024-08-12 04:18:54 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
   { name: "Home", link: "/" },
   { name: "About", link: "/about" },
-  { name: "Resume", link: "/resume.pdf" },
+  { name: "Resume", link: "/resume-12aug.pdf" },
   { name: "Contact", link: "/about#contact" },
 ];
 
